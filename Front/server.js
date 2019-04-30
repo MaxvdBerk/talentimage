53889e8d67a37a037f804f41eede808f6f47ac6b
Dennis van den Boom <dvandenboom@conclusion.nl>
2019-04-30 09:20:22 +0200

    new version front-end

--- Front/server.js
+++ Front/server.js
@@ -2,37 +2,52 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var mongoAPIURL = 'http://mongoapiv2-comedic-gazelle.eu-gb.mybluemix.net/post';
+// var cfenv = require("cfenv"); // nodig bij pushen naar cloud, voor vullen mongoAPIURL
 var request = require('request');
 var WatsonClient = require('./WatsonAPI/WatsonCall');
+var ejs = require("ejs");
+
+//Set global variables
 var port = process.env.PORT || 3000;
+var mongoAPIURL = 'https://sad-tiger.eu-gb.mybluemix.net/post'; //for local use
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, type: "application/json" }));
+app.set('view engine', 'ejs');
+
+// var appEnv = cfenv.getAppEnv();
+
+// mongoAPIURL = appEnv.getServiceURL(Mongo-API);
+// console.log(mongoAPIURL);
+
 
 // SET STORAGE
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
-//ROUTES WILL GO HERE
+//Define routes
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/upload/photo', upload.single('myImage'), (req, res) => {
     var file = req.file.buffer;
-    Watsonresponse = new Object;
-    console.log(Watsonresponse);
+    var fileSize = req.file.size / 1024 / 1024;
+    Watsonresponse = undefined;
     Result = new Object;
+    
 
     WatsonClient(file);
 
-    res.setTimeout(3000, function () {
+    res.setTimeout(5000, function () {
         console.log("to mongo " + Watsonresponse);
-        if (Watsonresponse === undefined) {
-            res.send("Image is not recognized")
-        } else if (Watsonresponse === {}){
-            res.send("Image is not recognized")
+        if (fileSize >= 10) {
+            res.render('error1')
+            res.end();
+        }    
+        else if (Watsonresponse === undefined) {
+            res.render('error2')
+            res.end();
         } else {
             request.post({
                 "headers": { "content-type": "application/json" },
@@ -43,15 +58,14 @@ app.post('/upload/photo', upload.single('myImage'), (req, res) => {
                     return console.dir(error);
                 }
                 Result = JSON.parse(body);
-                viewVariable1 = "Class = " + Result.Image.class + '</br>';
-                viewVariable2 = "Score = " + Result.Image.score + '</br>';
-                viewVariable3 = "Count = " + Result.Count;
-                res.send(viewVariable1 + viewVariable2 + viewVariable3);
+                viewVariable1 = Result.Image.class;
+                viewVariable2 = Result.Image.score;
+                viewVariable3 = Result.Count;
+                res.render('result', {Class: viewVariable1, Score: viewVariable2, Count: viewVariable3});
             });
         }
     });
 
 });
-
 
 app.listen(port, () => console.log(('Server started on port %d'), port));
